fabe2ff9bdff632bd04cf751880b9672eef906f5
gayanmora <gayanmoraliyage@gmail.com>
2020-06-20 21:48:14 +0530

    Use of a Broken or Risky Cryptographic Algorithm

--- MobileApp/src/components/UserDataHandling/UserDataHandling.js
+++ MobileApp/src/components/UserDataHandling/UserDataHandling.js
@@ -40,7 +40,7 @@ export function generateUUID() {
   let uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(
     c,
   ) {
-    let r = (dt + Math.random() * 16) % 16 | 0;
+    let r = (dt +  window.Crypto.getRandomValues(new Uint32Array(1)) * 16) % 16 | 0;
     dt = Math.floor(dt / 16);
     return (c == 'x' ? r : (r & 0x3) | 0x8).toString(16);
   });
